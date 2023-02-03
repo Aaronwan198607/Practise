5bf73f20bee17c4401ac78be0c1bf82eb454a8d0
”aaron” <helloaaron724@outlook.com>
2023-02-03 20:28:52 +0800

    修改querySelector內的文字

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 
 const el =document.querySelector('h1');
-//宣告一個el的變數,賦予一個值給el,dom的一個節點(document.querySelector),裡面的選擇器中h1的選擇器
+//宣告一個el的變數,賦予一個值給el,
+//dom的一個節點(document.querySelector), 裡面的選擇器中h1的選擇器
+el.textContent = "修改el內的文字";
+
+document.querySelector('h1').textContent = "把文字修改回來";
