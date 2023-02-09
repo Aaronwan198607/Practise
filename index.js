f6dbd13bebcf9052f48d35e13f0477e302c00bbb
Aaronwan198607 <116135929+Aaronwan198607@users.noreply.github.com>
2023-02-09 13:51:56 +0800

    新增ul>li標籤並在js內找到dom並新增連結

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ main.innerHTML = `<h1 class="header">標題</h1>`;
 //使用反引號原因是因為反引號可以插入字串及變數,
 //也比較不會和'' & " " 讓系統判讀不到
 
-const list = document.querySelector('.list');
+const list=document.querySelector('.list');
 console.log(list);
-
-
-list.innerHTML='<li><a href="http://www.google.com.tw">Google的連結</a></li>';
-
+list.innerHTML=`<li><a href="http://google.com.tw">Google的連結</a></li>`;
