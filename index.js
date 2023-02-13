4e2bbd990ecd2714e6f8b560db9755f1b77d380a
”aaron” <helloaaron724@outlook.com>
2023-02-13 23:58:50 +0800

    觸發監聽事件執行程序

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ console.log(btn,title);
 btn.addEventListener('click', function (e) {
     //addEventListener=>增加監聽事件函示
     console.log("click");
+    //往下執行修改文字的程序
+    title.textContent = "因按鈕點及觸發修改文字";
 });
 
 //另一種寫法 ↓ (但容易搞混,建議用上一個方式撰寫)
