bb42a84e3a8919ac5f8d929e14f78ef79ae76823
”aaron” <helloaaron724@outlook.com>
2023-02-13 20:51:22 +0800

    取屬性的內容

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
+const link = document.querySelector('a');
+console.log(link.getAttribute('href'));
+//宣告a標籤為變數link
+//印出link變數內的href屬性內的數值
 
-
-// const linkOne = document.querySelector('a');
-// console.log(linkOne);
-// linkOne.setAttribute("href", "www.yahoo.com.tw");
-
-const boxColor=document.querySelector('.box');
-console.log(boxColor);
-boxColor.setAttribute("style", "background-color:red");
-
-//setAttribute()可修改標籤內中之屬性質
-
-
-const yahooColor=document.querySelector("a");
-console.log(yahooColor);
-yahooColor.setAttribute("class","yahooColor");
-
-//新增標籤內之選擇棄並命名為yahooColor
-
-const linkName=document.querySelectorAll('a');
-console.log(linkName);
-
-//.querySelectorAll()可選取相同標籤屬性，會以陣列的方式做選取
-
-linkName[1].setAttribute("class", "red");
-//選取單一陣列的物件在[]內中填入陣列的順序
-
-
-
+let conten = link.innerHTML;
+console.log(conten);
 
