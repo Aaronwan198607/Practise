93c059d1a5b6a073b81712fe6e84987b464b972e
”aaron” <helloaaron724@outlook.com>
2023-02-14 00:17:23 +0800

    利用.addEventListener製作加法小程式

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const btn = document.querySelector('.btn');
 const title = document.querySelector('h1');
-console.log(btn,title);
+console.log(btn, title);
+let count = 0;
+//宣告count為數字0,
+// 不將let count = 0 放入function中
+//是因可以持續宣告不因為函示執行一次而消失宣告
 
 //監聽事件
 btn.addEventListener('click', function (e) {
     //addEventListener=>增加監聽事件函示
-    console.log("click");
-    //往下執行修改文字的程序
-    title.textContent = "因按鈕點及觸發修改文字";
-});
-
-//另一種寫法 ↓ (但容易搞混,建議用上一個方式撰寫)
-
-document.querySelector('.btn').addEventListener('click', function (e) {
-    console.log("click");
+    console.log(count);
+    //監聽點擊按鈕觸發count變數,將數字做加總顯示在console
+    count += 1;
+    title.innerHTML = count;
+    //修改title變數h1標籤
 });
