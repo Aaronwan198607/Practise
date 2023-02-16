2e9b39122b045ebed86532331dd420c60d8f7560
”aaron” <helloaaron724@outlook.com>
2023-02-16 21:31:25 +0800

    路徑重新設定

---
+++ 六角學院JavaScrip/0216/06.forEach讀取資料/App.js
@@ -0,0 +1,13 @@
+let data = [
+    {
+        name: 'Tom',
+        sex: "male",
+    },
+    {
+        name:'Merry',
+        sex: "woman",
+    }
+];
+data.forEach(function (item,index) {
+    console.log(item.name,item.sex);
+})
