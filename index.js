3f2f9389be30c8d4670b3e0142890e5f97816d9f
Aaronwan198607 <116135929+Aaronwan198607@users.noreply.github.com>
2023-02-16 12:05:46 +0800

    forEach

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const link = document.querySelector("a");
-console.log(link);
+// const link = document.querySelector("a");
+// console.log(link);
 
-link.addEventListener("click", (e) => {
-    e.preventDefault();
-    //preventDefault 取消默認值執行行為不跳轉網址
-    console.log("hello");
-    document.querySelector("h1").textContent = '不轉換網址';
+// link.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     //preventDefault 取消默認值執行行為不跳轉網址
+//     console.log("hello");
+//     document.querySelector("h1").textContent = '不轉換網址';
+// })
+
+let data=[10,20,30,40,50];
+let total=0
+data.forEach(function(item){
+    //執行5次forEach迴圈
+    total+=item;
+    //加總陣列數值
+    console.log(total);
 })
