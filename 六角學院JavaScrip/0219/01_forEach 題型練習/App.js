cb14446b5a4a86a44092e2433888389ce3085f50
”aaron” <helloaaron724@outlook.com>
2023-02-19 00:40:54 +0800

    openData查詢物件並重新定義

---
+++ 六角學院JavaScrip/0219/01_forEach 題型練習/App.js
@@ -0,0 +1,30 @@
+let data = [
+  {
+    Charge: "免費",
+    name: "廖洧杰充電站",
+  },
+  {
+    Charge: "投幣式",
+    name: "小花充電站",
+  },
+  {
+    Charge: "投幣式",
+    name: "小明充電站",
+  },
+  {
+    Charge: "投幣式",
+    name: "小天充電站",
+  },
+];
+
+const list=document.querySelector(".list");
+// console.log(list);
+let str="";
+data.forEach(function(item,index){
+    let contens=(`<li>${item.name},${item.Charge}</li>`);
+//NOTE:在此要先consol.log(contens),確認是否正常顯示
+    str+=contens;
+    console.log(str);
+})
+
+list.innerHTML=str;
