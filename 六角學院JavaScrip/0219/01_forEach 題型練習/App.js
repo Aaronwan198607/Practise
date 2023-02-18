b90597308094aae89c65971d86606d5c700ab967
”aaron” <helloaaron724@outlook.com>
2023-02-19 02:11:45 +0800

    事件監聽搭配 if 流程判斷

--- 六角學院JavaScrip/0219/01_forEach 題型練習/App.js
+++ 六角學院JavaScrip/0219/01_forEach 題型練習/App.js
@@ -36,13 +36,26 @@ init();
 const filter = document.querySelector(".filter");
 console.log(filter);
 filter.addEventListener("click", (e) => {
-//   console.log(e.target.value==undefined);
-  //NOTE:加上unfefined主要是因為div區塊較廣,點到旁邊的位置會顯示undifine
-  //NOTE:為了判斷是不是確實有點到按鈕,而不是因觸發倒不是按鈕的區塊
+    //   console.log(e.target.value==undefined);
+    //NOTE:加上unfefined主要是因為div區塊較廣,點到旁邊的位置會顯示undifine
+    //NOTE:為了判斷是不是確實有點到按鈕,而不是因觸發倒不是按鈕的區塊
     if (e.target.value == undefined) {
         console.log('不是點到按鈕而點到其他的地方了!!');
         return;
         //POINT:終止函式執行,不跑後續的邏輯程式
     }
-    console.log(e.target.value);
-})
+    let str = "";
+    data.forEach(function (item, index) {
+        console.log(e.target.value == item.Charge);
+        //NOTE:確認點擊的按鈕有對應到物件裡面的資料,將其consol.log出來
+        if (e.target.value == item.Charge) {
+            str += `<li>${item.name},${item.Charge}</li>`
+            //POINT:NOTE:將#47宣告的變數str賦予值
+            console.log(str)
+        }
+    })
+    const list = document.querySelector('.list');
+    list.innerHTML = str;
+ //POINT:重新賦予值時innerHTML會清空ˋ物件值並重新將字串#52行帶入
+
+});
